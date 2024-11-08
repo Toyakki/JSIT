cea3a0a8630fe9447bb7a5bbb4b452df5cfc8aef
theflickerman <whippetkid@yahoo.ca>
2024-11-08 17:33:15 -0500

    added Account entity and AccountFactory

--- src/java/entities/AccountFactory.java
+++ src/java/entities/AccountFactory.java
@@ -1,4 +1,11 @@
 package entities;
 
 public class AccountFactory {
+    private AccountFactory() {
+
+    }
+
+    public static Account createAccount(String email, String password, String type){
+        return new Account(email, password, type);
+    }
 }
