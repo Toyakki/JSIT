85ce9b68ad9ebe3100c08ff0e3b85766c759df6d
theflickerman <whippetkid@yahoo.ca>
2024-11-13 21:23:08 -0500

    added AssignmentFactory file and renamed the course entities

--- src/java/entities/CourseFactory.java
+++ src/java/entities/CourseFactory.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 import java.util.UUID;
 
-public class ClassFactory {
-    public static Class createClass(String instructor, String className, ArrayList<String> students){
-        return new Class(
+public class CourseFactory {
+    public static Course createClass(String instructor, String className, ArrayList<String> students){
+        return new Course(
                 instructor,
                 className,
                 UUID.randomUUID().toString().replace("-", "").substring(0, 3),
