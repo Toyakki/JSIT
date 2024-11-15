0717d12ed7cd78d331bdac32f3611e61e3291719
theflickerman <whippetkid@yahoo.ca>
2024-11-15 17:35:00 -0500

    added menu to display clickable classes with action listeners programmatically, though the UI certainly needs some work

--- src/java/view/StudentClassesView.java
+++ src/java/view/StudentClassesView.java
@@ -6,6 +6,8 @@ import interface_adapters.student.StudentClassesViewModel;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.List;
@@ -14,13 +16,15 @@ public class StudentClassesView extends JPanel implements ActionListener, Proper
     private final String viewName = "student classes";
     private final StudentClassesViewModel classesViewModel;
     private final JLabel noCoursesLabel = new JLabel("No courses joined yet.");
-    private final JLabel titleLabel = new JLabel("Classes:");
+
+    private final JMenuBar menuBar = new JMenuBar();
+    private final JMenu coursesMenu = new JMenu("Courses");
 
     public StudentClassesView(StudentClassesViewModel viewModel) {
         this.classesViewModel = viewModel;
-
         this.classesViewModel.addPropertyChangeListener(this);
 
+        menuBar.add(coursesMenu);
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     }
 
@@ -42,10 +46,10 @@ public class StudentClassesView extends JPanel implements ActionListener, Proper
             add(noCoursesLabel);
         } else if (!this.classesViewModel.getState().getCourseNames().isEmpty()) {
             remove(this.noCoursesLabel);
-            add(this.titleLabel);
+            add(menuBar);
             List<String> courseNames = this.classesViewModel.getState().getCourseNames();
             for (String courseName : courseNames) {
-                add(createCourseLabel(courseName));
+                coursesMenu.add(renderCourse(courseName));
             }
         }
     }
@@ -54,8 +58,14 @@ public class StudentClassesView extends JPanel implements ActionListener, Proper
         return viewName;
     }
 
-    private JLabel createCourseLabel(String courseName) {
-        return new JLabel(courseName);
+    private JMenuItem renderCourse(String courseName) {
+        JMenuItem course = new JMenuItem(courseName);
+        course.addActionListener(
+                (e) -> {
+                    System.out.println(courseName);
+                }
+        );
+        return course;
     }
 
 }
