4ef14a42035961a1b956332d26b208a41d997c15
theflickerman <whippetkid@yahoo.ca>
2024-11-15 17:49:18 -0500

    swapped menu implementation for homemade jlabel and mouse listener implementation, looks significantly better now and avoids issues with needing to access the main frame

--- src/java/view/StudentClassesView.java
+++ src/java/view/StudentClassesView.java
@@ -4,10 +4,8 @@ import interface_adapters.student.StudentClassesState;
 import interface_adapters.student.StudentClassesViewModel;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
+import java.awt.*;
+import java.awt.event.*;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.List;
@@ -16,15 +14,12 @@ public class StudentClassesView extends JPanel implements ActionListener, Proper
     private final String viewName = "student classes";
     private final StudentClassesViewModel classesViewModel;
     private final JLabel noCoursesLabel = new JLabel("No courses joined yet.");
-
-    private final JMenuBar menuBar = new JMenuBar();
-    private final JMenu coursesMenu = new JMenu("Courses");
+    private final JLabel titleLabel = new JLabel("Classes:");
 
     public StudentClassesView(StudentClassesViewModel viewModel) {
         this.classesViewModel = viewModel;
         this.classesViewModel.addPropertyChangeListener(this);
 
-        menuBar.add(coursesMenu);
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     }
 
@@ -46,10 +41,10 @@ public class StudentClassesView extends JPanel implements ActionListener, Proper
             add(noCoursesLabel);
         } else if (!this.classesViewModel.getState().getCourseNames().isEmpty()) {
             remove(this.noCoursesLabel);
-            add(menuBar);
+            add(titleLabel);
             List<String> courseNames = this.classesViewModel.getState().getCourseNames();
             for (String courseName : courseNames) {
-                coursesMenu.add(renderCourse(courseName));
+                add(renderCourse(courseName));
             }
         }
     }
@@ -58,14 +53,15 @@ public class StudentClassesView extends JPanel implements ActionListener, Proper
         return viewName;
     }
 
-    private JMenuItem renderCourse(String courseName) {
-        JMenuItem course = new JMenuItem(courseName);
-        course.addActionListener(
-                (e) -> {
-                    System.out.println(courseName);
-                }
-        );
-        return course;
+    private JLabel renderCourse(String courseName) {
+        JLabel courseLabel = new JLabel(courseName);
+        courseLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e){
+                System.out.println(courseName);
+            }
+        });
+        return courseLabel;
     }
 
 }
