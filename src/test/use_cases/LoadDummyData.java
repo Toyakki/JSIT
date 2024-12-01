1ffabdc06b35178cf60e819fde06fb3a7891895e
sark <sark.asadourian@icloud.com>
2024-12-01 17:33:47 -0500

    fixed pom file to add only newest version of junit. Added test for TeacherCourseBackInteractor.

---
+++ src/test/use_cases/LoadDummyData.java
@@ -0,0 +1,60 @@
+package use_cases;
+
+import data_access.InMemoryUserDataAccessObject;
+import data_access.UserDataAccessInterface;
+import entities.Account;
+import entities.Assignment;
+import entities.Course;
+import entities.CourseFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoadDummyData {
+
+    public void loadData(UserDataAccessInterface demo) {
+        Course csCourse = CourseFactory.createClass("Johnathon Calver", "CSC207");
+        Course statsCourse = CourseFactory.createClass("Jeff Rosento", "STA257");
+        Course matCourse = CourseFactory.createClass("Joe Repka", "MAT347");
+        Course matCourseTwo = CourseFactory.createClass("Joe Repka", "MAT348");
+
+        List<Course> joe_courses = new ArrayList<>();
+        joe_courses.add(matCourse);
+        joe_courses.add(matCourseTwo);
+
+        List<Course> tohya_courses = new ArrayList<>();
+        tohya_courses.add(statsCourse);
+        tohya_courses.add(csCourse);
+
+        List<Course> test_courses = new ArrayList<>();
+        test_courses.add(statsCourse);
+        test_courses.add(csCourse);
+        test_courses.add(matCourse);
+
+
+        Account joe = new Account("joerepka@mail.utoronto.ca", "algebra", "teacher", joe_courses);
+        Account tohya = new Account("henrik-ibsen707@gmail.com", "thewildduck", "student", tohya_courses);
+        Account test = new Account("t", "t", "teacher", test_courses);
+        demo.saveUser(joe);
+        demo.saveUser(tohya);
+        demo.saveUser(test);
+
+        Assignment ps1 = new Assignment(matCourse,"Problem Set 1", "December 1st", "100", "graded", "true");
+        Assignment ps2 = new Assignment(matCourse,"Problem Set 2", "December 15st", "100", "assigned", "false");
+        Assignment ps3 = new Assignment(matCourse,"Problem Set 3", "December 15st", "100", "assigned", "false");
+        Assignment ps4 = new Assignment(matCourse, "Problem Set 4", "December 15st", "100", "assigned", "false");
+        Assignment ps5 = new Assignment(matCourse, "Problem Set 5", "December 15st", "100", "assigned", "false");
+        Assignment ps6 = new Assignment(matCourse, "Problem Set 6", "December 15st", "100", "assigned", "false");
+        Assignment ps7 = new Assignment(matCourse, "Problem Set 7", "December 15st", "100", "assigned", "false");
+        Assignment finalProject = new Assignment(csCourse, "Final Project", "December 4th", "25", "submitted", "false");
+
+        csCourse.addAssignment(finalProject);
+        matCourse.addAssignment(ps1);
+        matCourse.addAssignment(ps2);
+        matCourse.addAssignment(ps3);
+        matCourse.addAssignment(ps4);
+        matCourse.addAssignment(ps5);
+        matCourse.addAssignment(ps6);
+        matCourse.addAssignment(ps7);
+    }
+}
---
+++ src/test/use_cases/teacher_course_back_interactor/TeacherCourseBackInteractorTest.java
@@ -0,0 +1,54 @@
+package use_cases.teacher_course_back_interactor;
+
+import data_access.InMemoryUserDataAccessObject;
+import data_access.UserDataAccessInterface;
+import interface_adapters.ViewManagerModel;
+import interface_adapters.teacher.TeacherClassesViewModel;
+import interface_adapters.teacher.TeacherCoursesPresenter;
+import org.junit.jupiter.api.Test;
+import use_cases.LoadDummyData;
+import use_cases.UserOutputData;
+import use_cases.teacher_course_back.TeacherCourseBackInputBoundary;
+import use_cases.teacher_course_back.TeacherCourseBackOutputBoundary;
+import use_cases.teacher_course_back.TeacherCourseBackUseCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// Tests TeacherCourseBackInteractor with a teacher with 2 courses checking if the UserOutputData is formated as expected.
+public class TeacherCourseBackInteractorTest {
+
+    @Test
+    void create() {
+        // Load Data
+        UserDataAccessInterface userDsGateway = new InMemoryUserDataAccessObject();
+        LoadDummyData loadDummyData = new LoadDummyData();
+        loadDummyData.loadData(userDsGateway);
+        // Create classes needed to run the use case
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        TeacherClassesViewModel teacherClassesViewModel = new TeacherClassesViewModel();
+        TeacherCourseBackOutputBoundary teacherCourseBackOutputBoundary = new TeacherCoursesPresenter(viewManagerModel, teacherClassesViewModel);
+        // Creates an interactor with a modified output bound to test if the UserOutputData is formated as expected.
+        TeacherCourseBackInputBoundary interactor = getTeacherCourseBackInputBoundary(userDsGateway, teacherCourseBackOutputBoundary);
+
+        interactor.back("joerepka@mail.utoronto.ca");
+    }
+
+    private static TeacherCourseBackInputBoundary getTeacherCourseBackInputBoundary(UserDataAccessInterface userDsGateway,
+                                                                                    TeacherCourseBackOutputBoundary teacherCourseBackOutputBoundary) {
+            TeacherCourseBackOutputBoundary courseBackOutputBoundary = new TeacherCourseBackOutputBoundary() {
+            @Override
+            public void prepareTeacherCoursesView(UserOutputData userOutputData) {
+                assertEquals("teacher", userOutputData.getType());
+                assertEquals("joerepka@mail.utoronto.ca", userOutputData.getEmail());
+                List<String> courseNames = new ArrayList<String>();
+                courseNames.add("MAT347");
+                courseNames.add("MAT348");
+                assertEquals(courseNames, userOutputData.getCourseNames());
+            }
+        };
+        return new TeacherCourseBackUseCase(userDsGateway, courseBackOutputBoundary);
+    }
+}
